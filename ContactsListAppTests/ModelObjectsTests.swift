c34944ed3d7fe6987a5fecf9655f434ab7a20126
Damian Modernell <damcho@gmail.com>
2018-11-16 20:39:11 -0300

    ContactModel unit testing

---
+++ ContactsListAppTests/ModelObjectsTests.swift
@@ -0,0 +1,72 @@
+//
+//  ContactsListAppTests.swift
+//  ContactsListAppTests
+//
+//  Created by Damian Modernell on 15/11/2018.
+//  Copyright © 2018 Damian Modernell. All rights reserved.
+//
+
+import XCTest
+@testable import ContactsListApp
+
+class ModelObjectsTests: XCTestCase {
+
+    let dateFormatter = DateFormatter()
+
+    
+    override func setUp() {
+     
+    }
+    
+    func createContact() -> ContactModel {
+        let contact = ContactModel()
+        contact.biography = "some biography"
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        contact.born = dateFormatter.date(from: "12/12/1222")
+        contact.name = "some name"
+        contact.email = "some@email.com"
+        return contact
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testContactModelValidation() {
+        var error:NSError?
+        var contact:ContactModel
+        contact = createContact()
+        error = contact.validate() as NSError?
+        XCTAssertNil(error)
+        
+        contact = createContact()
+        contact.name = ""
+        error = contact.validate() as NSError?
+        XCTAssertEqual(error?.code,0 )
+        
+        contact = createContact()
+        contact.email = "someemail@email"
+        error = contact.validate() as NSError?
+        XCTAssertEqual(error?.code,1)
+        
+        contact = createContact()
+        contact.born = dateFormatter.date(from:"12/12/")
+        error = contact.validate() as NSError?
+        XCTAssertEqual(error?.code,2 )
+        
+        contact = createContact()
+        contact.biography = ""
+        error = contact.validate() as NSError?
+        XCTAssertEqual(error?.code,3 )
+    }
+    
+    func testContactEquality() {
+        let contact1 = createContact()
+        let contact2 = createContact()
+        XCTAssertEqual(contact1, contact2)
+        
+        contact2.email = "different@email.com"
+        XCTAssertNotEqual(contact2, contact1)
+    }
+
+}
